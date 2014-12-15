124b4f318ac022760f2dd59ccba8be5145e441c7
Shuhei Nomura <info@danmaq.com>
2014-12-15 02:15:24 +0000

    Stateパターン追加

---
+++ GoFExperienceCS/Behavioral/State/Context.cs
@@ -0,0 +1,26 @@
+﻿namespace GoFExperienceCS.Behavioral.State
+{
+	static class Context
+	{
+
+		public interface IState
+		{
+			Func<IContext, IContext> Setup(IContext context);
+		}
+
+		public interface IContext
+		{
+
+			Func<IContext, IContext> State
+			{
+				get;
+			}
+
+			IState NextState
+			{
+				get;
+			}
+		}
+
+	}
+}
